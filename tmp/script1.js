0f7fdf9b2331495df9a4e54412032a2742e302c3
byr-gdp <anthozoan77@gmail.com>
2015-12-25 15:05:29 +0800

    first commit

---
+++ tmp/script1.js
@@ -0,0 +1,2 @@
+javascript:(function(){
+  var script=document.createElement('script');script.setAttribute('src','http://127.0.0.1:7777/index.js');document.getElementsByTagName('head')[0].appendChild(script);})();
